519f072e3319678b984a59cf419f43c5d3d1e2fd
Ayush Jha <ayush@Ayushs-MacBook-Air.local>
2026-06-07 19:59:24 +0530

    disable early hints to fix browser loading

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/*": ["./public/**/*"],
   },
+  experimental: {
+    earlyHints: false,
+  },
 };
 
 export default nextConfig;
